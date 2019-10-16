eca57a3d2d4a8ca5bfd87f1e01367e3b8facc588
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-16 20:46:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
   # complexity.
   {railroads: {
     pieces: 4,
-    rent_in_dollars: {1: "", 2: "", 3: "", 4: ""},
+    rent_in_dollars: "", #{1: "", 2: "", 3: "", 4: ""},
     key3: "",
     key4: ""
   }}
