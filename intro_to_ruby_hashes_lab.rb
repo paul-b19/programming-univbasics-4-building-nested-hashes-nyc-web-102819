7d8877048d14ef97e8a5c49854d83ae55c4bb4ac
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-16 20:56:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
     :pieces => 4,
     :rent_in_dollars => {
       one_piece_owned: 25,
-      two_piece_owned: 50,
+      two_pieces_owned: 50,
       c: "",
       d: ""},
     :names => {a: "", b: "", c: "", d: ""}
