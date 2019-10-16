4d5939a649c966ccc973165caf2879cbae56e138
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-16 21:00:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,10 +24,11 @@ def monopoly_with_third_tier
       two_pieces_owned: 50,
       three_pieces_owned: 100,
       four_pieces_owned: 200},
-    :names => {reading_railroad: {},
-    b: "",
-    c: "",
-    d: ""}
+    :names => {
+      reading_railroad: {},
+      pennsylvania_railroad: {},
+      c: "",
+      d: ""}
   }}
 end
 
